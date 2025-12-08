0c082c633bf1b652c67b8800bb1a423c93c03303
ken-333 <3605188700@qq.com>
2025-12-07 23:55:05 -0500

    create config and model_builder

---
+++ config.py
@@ -0,0 +1,75 @@
+# ==========================
+# 1.physicial parameters
+# ==========================
+# accroding to the R250M2 device
+P_TOTAL_RETED = 6500.0 #W(设备总额定功率)
+P_LASER_MAX = 1000.0   #W（双激光 500W*2）
+P_BASE = P_TOTAL_RETED - P_LASER_MAX #W（基座功率,非激光能耗：加热、风扇、运动系统、控制柜）
+
+# material properties (SS-CX stainless stell)
+RHO = 7.7e-6 #kg/mm^3 (密度: 7.70 g/cm^3)
+
+# carbon emission factors
+EF_ELEC = 1.49e-4 #kgCO2/kJ (即 0.5366 kgCO2/kWh)
+EF_POWDER = 1.45  #kgCO2/kg
+# ==========================
+# 2.economic parameters
+# ==========================
+# gas consumption
+GAS_FLOW_RATE = 3.0 / 60.0  # L/s (3 L/min)
+PRICE_GAS = 0.053           # Yuan/L (53 Yuan/m^3)
+C_GAS_SCE = GAS_FLOW_RATE * PRICE_GAS  # Yuan/s (每秒气体消耗成本)
+
+# machine and labor cost
+C_MACHINE = 154.0 / 3600.0  # Yuan/s (154 Yuan/hour)
+C_LABOR = 60.0 / 3600.0    # Yuan/s (60 Yuan/hour)
+
+#integrated time cost coefficient (CNY/s)
+# COST_TIME = C_MACHINE + C_LABOR + C_GAS_SCE
+COST_TIME_TOTAL = C_MACHINE + C_LABOR + C_GAS_SCE
+
+# ==========================
+# 3.stochastic parameters
+# ==========================
+# "Two-stage Stochastic Programming"
+SCENERIOS = [
+    # Scenerio 1:pessimistic
+    {'probability': 0.25, 'loss_rate': 0.16,'price':110},
+    # Scenerio 2:normal
+    {'probability': 0.50, 'loss_rate': 0.13,'price':100},
+    # Scenerio 3:optimistic
+    {'probability': 0.25, 'loss_rate': 0.10,'price':90},
+]
+
+# ==========================
+# 4.post-processing trade-off 后处理权衡
+# ==========================
+# 这是制造 "成本 vs 效率" 冲突的关键。
+# 逻辑：层厚越大，表面越粗糙，后处理成本越高。
+# 单位: Yuan/mm^3 (打印体积)
+POST_COST_MAP = {
+    80: 0.020,  # for 80 um  20 Yuan/cm3 (基准)
+    100: 0.025, # for 100 um 25 Yuan/cm3 (增加 25%)
+    120: 0.035, # for 120 um 35 Yuan/cm3 (增加 75%) 惩罚大层厚的粗糙度
+}
+
+# ==========================
+# 5.variables and solver settings
+# ==========================
+BOUND = {
+    'P':(385,460),         #W (laser power)
+    'V':(700,1150),       #mm/s (scan speed)
+    'E':(90,115),         #J/mm3 (volumetric energy density)
+}
+
+# constraint
+ED_MIN = 30  #J/mm3 (目标能量密度)
+ED_MAX = 70 #J/mm3 (目标能量密度)
+
+#GUROBI settings
+# NonConvex =2 this is the crucial parameter to deal with non-linear problems
+SOLVER_OPTS = {
+    'NomConvex': 2, # 开启非凸二次规划支持
+    'MIPGap': 0.01, # 1% optimality gap
+    'TimeLimit': 300 # seconds 限制每个点算5分钟，防止卡死
+}
---
+++ model_builder.py
@@ -0,0 +1,84 @@
+from pyomo.environ import *
+import config as cfg
+
+def create_lpbf_model(lt_val_um): # 输入参数是（layer thickness）lt_val_um: 层厚 (单位: um)，例如 80, 100, 120
+ # 1.Initialize the model
+ m = ConcreteModel()
+ # ==========================
+ # 2.Define decision variables
+ # ==========================
+ # variables range from config.py BOUND
+ m.p = Var(bounds=cfg.BOUND['P'],domain=Reals,name="Power_W")  #Var(...) 是 Pyomo 创建决策变量的函数/类。
+ m.v = Var(bounds=cfg.BOUND['V'],domain=Reals,name="Speed_mm_s")
+ m.H = Var(bounds=cfg.BOUND['E'],domain=Reals,name="Hatch_um") 
+
+ # ==========================
+ # 3.Define auxiliary variable
+ # ==========================
+ # 3.1 单位转换: H (um) -> H_mm (mm)
+ # 这是一个线性约束: H_mm = H / 1000
+ m.H_mm = Var(domain=Reals)
+ m.con_unit_h = Constraint(expr= m.H_mm == m.H / 1000.0)
+
+ # 3.2 层厚单位转换：lt_val_um → lt_mm（常数）
+ lt_mm = lt_val_um /1000.0   #注意 lt_mm 是一个普通的 Python 浮点数，不是 Pyomo 变量。
+
+ #define bulid rate (BuildRate = V * H_mm * LT_mm)
+ # 这是一个二次非线性项 (V * H_mm)，因为 LT_mm 是常数
+ m.BuildRate = Var(domain=NonNegativeReals, name="Inverse_BuildRate")
+ 
+ #注意: Gurobi 需要特定语法来识别这种约束，但在 Pyomo 中我们先写成非线性形式
+ # 只要在求解时开启 NonConvex=2，Gurobi 会自动处理这种 xy=1 的关系
+ m.con_inv_rate = Constraint(expr= m.InvRate * m.BuildRate == 1.0)
+ #构建速率的倒数InvRate，表示单位体积所需时间（s/mm³）
+ 
+ # ==========================
+ # 4.Define objective functions
+ # ==========================
+ # 必须使用 ObjectiveList，这是 PyAugmecon 的要求
+ m.obj_list= ObjectiveList() #ObjectiveList  Pyomo 中用于存多个目标函数的容器（因为要做多目标优化）
+
+ # calculate the expected materials cost and carbon emission
+ expected_mat_cost = 0.0
+ expected_mat_carbon = 0.0
+ 
+ for s in cfg.SCENARIOS:
+  # 材料成本 = 密度 * (1+损耗) * 价格
+  mat_cost = cfg.RHO * (1+ s['loss_rate']) * s['price']  # Yuan/mm³
+  expected_mat_cost += s['prob'] * mat_cost
+
+ # 材料碳排放 = 密度 * (1+损耗) * 因子
+ mat_carbon = cfg.RHO * (1 + s['loss_rate']) * cfg.EF_POWDER  # kgCO2/mm³
+ expected_mat_carbon += s['prob'] * mat_carbon
+
+ # Obtain the post-processing cost corresponding to the current layer thickness (conflicting item)
+ post_cost = cfg.POST_COST_MAP[lt_val_um] #根据当前层厚 lt_val_um，查找对应的后处理成本。
+ 
+ # ==========================
+ # Objective 1: Minimize total cost (MAX -cost)
+ # ==========================
+ expr_cost = cfg.C_TIME_TOTAL * m.InvRate + expected_mat_cost + post_cost
+ m.obj_list.add(expr=-1 * expr_cost, sense=maximize)
+
+ # ==========================
+ # Objective 2: Minimize total carbon emission (MAX -carbon)
+ # ==========================
+ # P_total = P_laser (variable) + P_base (constant 5500)
+ # 这是一个二次项 (P * InvRate)，Gurobi 可解
+ expr_carbon=((m.p +cfg.P_BASE) * cfg.EF_ELEC * m.InvRate) + expected_mat_carbon
+ m.obj_list.add(expr=-1 * expr_carbon, sense=maximize)
+
+ # ==========================
+ # Objective 3: MAXimize efficiency (BuildRate)
+ # ==========================
+ m.obj_list.add(expr = m.BuildRate, sense = maximize)
+ 
+
+ # ==========================
+ # constriant 
+ # ==========================
+ # ED>=10 AND ED<=70 线性化为: 30 * BuildRate <= P <= 70 * BuildRate
+ m.con_ed_min = Constraint(expr= m.p >= cfg.ED_MIN * m.BuildRate)
+ m.con_ed_max = Constraint(expr= m.p <= cfg.ED_MAX * m.BuildRate)
+
+ return m
